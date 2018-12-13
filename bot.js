67095e51cf2e3a55c3416fbc7074d360ce2c913f
` 0ff <39999036+00F@users.noreply.github.com>
2018-12-13 16:45:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const developers = ['495533707512250389','320423357709549568'];
+const developers = ['450829132343934997','320423357709549568'];
 
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame(`--`,'https://www.twitch.tv/MeeRcY')
+  client.user.setGame(`-`,'https://www.twitch.tv/MeeRcY')
   client.user.setStatus("idle")
 });
 
@@ -13,10 +13,14 @@ client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!developers.includes(message.author.id)) return;
 
-if (message.content.startsWith('.St')) {
+if (message.content.startsWith('.Play')) {
+client.user.setActivity(argresult, {type:'Playing'});
+    message.channel.send(` تم تغير البلاينق ألى **${argresult} \** `)
+} else 
+if (message.content.startsWith('.st')) {
   client.user.setGame(argresult, "https://www.twitch.tv/meercy");
-   message.channel.send(`${argresult} \` `)
+   message.channel.send(` تم تغير الستريمنق ألى **${argresult} \** `)
 }
 });
 
-client.login(process.env.HERO);
+client.login(process.env.BOT_TOKEN);
